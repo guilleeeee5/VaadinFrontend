8a8404a3458bf839cf394ea927b55012dd1877c4
jmgarciab2 <jmgarciab2@gmail.com>
2022-12-22 14:44:36 +0100

    Creado el dialog con todas las etiquetas y los textos que se van a mostrar posteriormente desde el grid

--- src/main/java/org/vaadin/example/MainView.java
+++ src/main/java/org/vaadin/example/MainView.java
@@ -5,7 +5,9 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
@@ -43,7 +45,38 @@ public class MainView extends VerticalLayout{
     public MainView() {
 
         ArrayList<ZonaBasicaSalud> listaPacientes = new ArrayList<>();
+        Dialog dialog = new Dialog();
+        dialog.setHeight("800");
+        dialog.setWidth("300");
+        dialog.getElement().setAttribute("aria-label", "Mostrar/editar Zonas");
+        VerticalLayout verticalLayout = new VerticalLayout();
+        HorizontalLayout horizontalLayout1 = new HorizontalLayout();
+        HorizontalLayout horizontalLayout2 = new HorizontalLayout();
+        Label etiqueta1 = new Label("Codigo geometria");
+        TextField texto1 = new TextField();
+        Label etiqueta2 = new Label("Zona basica salud");
+        TextField texto2 = new TextField();
+        Label etiqueta3 = new Label("Tasa 14 dias");
+        TextField texto3 = new TextField();
+        Label etiqueta4 = new Label("Tasa acumulada total");
+        TextField texto4 = new TextField();
+        Label etiqueta5 = new Label("Casos totales");
+        TextField texto5 = new TextField();
+        Label etiqueta6 = new Label("Casos 14 dias");
+        TextField texto6 = new TextField();
+        Label etiqueta7 = new Label("Fecha informe");
+        TextField texto7 = new TextField();
+        Label etiqueta8 = new Label("Fecha final");
+        TextField texto8 = new TextField();
 
+        horizontalLayout1.add(etiqueta1, texto1, etiqueta2, texto2, etiqueta3, texto3, etiqueta4, texto4);
+        horizontalLayout1.setAlignItems(Alignment.CENTER);
+
+        horizontalLayout2.add(etiqueta5, texto5, etiqueta6, texto6, etiqueta7, texto7, etiqueta8, texto8);
+        horizontalLayout2.setAlignItems(Alignment.CENTER);
+
+        verticalLayout.add(horizontalLayout1, horizontalLayout2);
+        dialog.add(verticalLayout);
        
     }
 
