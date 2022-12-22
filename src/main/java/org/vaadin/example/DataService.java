ad23325c71c34c796bbfa3c1c5a071f6cc914967
jmgarciab2 <jmgarciab2@gmail.com>
2022-12-22 14:35:22 +0100

    Creada la url de recogida de datos

--- src/main/java/org/vaadin/example/DataService.java
+++ src/main/java/org/vaadin/example/DataService.java
@@ -1,2 +1,6 @@
-package org.vaadin.example;public class DataService {
+package org.vaadin.example;
+
+public class DataService {
+    private static final String urlPrefix = "http://localhost:8080/ZonaBasicaSalud";
+
 }
